910e8a33de1b87090818537c5ad060776f712b12
David Aaron DiManna <ddimanna@users.noreply.github.com>
2016-10-22 21:20:51 +0000

    added the playerController script, as well as the camera controls script and the moving platform and destroyPickup scripts

---
+++ NanashiProject/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+	public Transform target;
+
+	public float borderX = 1.6f;
+
+	public float borderY = 0.5f;
+
+	public float smoothTime = 0.2f;
+
+	public bool useSmoothing = true;
+
+	private Transform playerTransform;
+
+	private Vector2 velocity;
+
+	private void Start()
+	{
+		playerTransform = transform;
+	}
+
+	private void Update()
+	{
+		Vector2 zero = Vector2.zero;
+		if (useSmoothing)
+		{
+			zero.x = Mathf.SmoothDamp(playerTransform.position.x, target.position.x + borderX, ref velocity.x, smoothTime);
+			zero.y = Mathf.SmoothDamp(playerTransform.position.y, target.position.y + borderY, ref velocity.y, smoothTime);
+		}
+		Vector3 b = new Vector3(zero.x, zero.y, transform.position.z);
+		transform.position = Vector3.Slerp(transform.position, b, Time.time);
+	}
+}
+
---
+++ NanashiProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+	public float maxSpeed = 10f;
+
+	private bool facingRight = true;
+
+	public bool grounded;
+
+	public bool wall;
+
+	public float jumpForce = 700f;
+
+	public float dashForce = 1f;
+
+	public bool doubleJump;
+
+	private bool dashing;
+
+	private bool isClimbing;
+
+	public Rigidbody2D rigi;
+
+	public Transform groundCheck;
+
+	public Transform wallCheck;
+
+	private float groundRadius = 0.2f;
+
+	private float wallRadius = 0.2f;
+
+	public LayerMask whatIsGround;
+
+	public LayerMask whatIsWall;
+
+	private Vector2 temp;
+
+	private int jumpCount;
+
+	public bool doubleJumpPickup;
+
+	public bool dashPickup;
+
+	public bool wallClimbPickup;
+
+	private bool canDash = true;
+
+	private float dashCoolDown = 1f;
+
+	private Animator anim;
+
+	private void Start()
+	{
+		rigi = GetComponent<Rigidbody2D>();
+		temp.y = rigi.velocity.y * 0f;
+		anim = GetComponent<Animator>();
+		doubleJumpPickup = false;
+		dashPickup = false;
+		wallClimbPickup = false;
+	}
+
+	private void FixedUpdate()
+	{
+		float move = Input.GetAxis("Horizontal");
+
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+		wall = Physics2D.OverlapCircle(wallCheck.position, wallRadius, whatIsWall);
+
+		anim.SetBool("Ground", grounded);
+		//anim.SetFloat("vSpeed", rigi.velocity.y);
+		anim.SetFloat("Speed", Mathf.Abs(move));
+
+		if (!dashing)
+		{
+			rigi.velocity = new Vector2( move * maxSpeed, rigi.velocity.y);
+			if (move > 0f && !facingRight)
+			{
+				this.Flip();
+			}
+			else if (move < 0f && facingRight)
+			{
+				this.Flip();
+			}
+		}
+	}
+
+	private void Update()
+	{
+//		if (Input.GetButtonDown("Select"))
+//		{
+//			SceneManager.LoadScene(1);
+//		}
+//
+//		if (Input.GetButtonDown("Start"))
+//		{
+//			SceneManager.LoadScene(0);
+//		}
+
+
+
+		if (!grounded && canDash && facingRight && Input.GetButtonDown("Dash"))
+		{
+			if (dashPickup)
+			{
+			//	StartCoroutine(rightDash(0.3f));
+			}
+		}
+		else if (!grounded && canDash && !facingRight && Input.GetButtonDown("Dash") && dashPickup)
+		{
+			//StartCoroutine(leftDash(0.3f));
+		}
+
+
+		if (Input.GetButtonDown("Jump"))
+		{
+			if (grounded)
+			{
+				rigi.velocity = temp;
+				rigi.AddForce(new Vector2(0f, jumpForce));
+				doubleJump = true;
+			}
+			else if (doubleJumpPickup && doubleJump)
+			{
+				doubleJump = false;
+				rigi.velocity = temp;
+				rigi.AddForce(new Vector2(0f, jumpForce));
+			}
+		}
+
+
+		if (wall && Input.GetAxis("Grab") > 0f)
+		{
+			if (wallClimbPickup)
+			{
+				rigi.velocity = rigi.velocity * 0f;
+				wallClimb();
+			}
+		}
+		else if ((!wall || Input.GetAxis("Grab") <= 0f) && isClimbing)
+		{
+			rigi.gravityScale = 1f;
+			isClimbing = false;
+		}
+	}
+
+
+	private void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 localScale = transform.localScale;
+		localScale.x *= -1f;
+		transform.localScale = localScale;
+	}
+
+
+
+//	public IEnumerator rightDash(float dashDuration)
+//	{
+//		PlayerController.<rightDash>c__Iterator0 <rightDash>c__Iterator = new PlayerController.<rightDash>c__Iterator0();
+//		<rightDash>c__Iterator.dashDuration = dashDuration;
+//		<rightDash>c__Iterator.<$>dashDuration = dashDuration;
+//		<rightDash>c__Iterator.<>f__this = this;
+//		return <rightDash>c__Iterator;
+//	}
+//
+//
+//	public IEnumerator leftDash(float dashDuration)
+//	{
+//		PlayerController.<leftDash>c__Iterator1 <leftDash>c__Iterator = new PlayerController.<leftDash>c__Iterator1();
+//		<leftDash>c__Iterator.dashDuration = dashDuration;
+//		<leftDash>c__Iterator.<$>dashDuration = dashDuration;
+//		<leftDash>c__Iterator.<>f__this = this;
+//		return <leftDash>c__Iterator;
+//	}
+
+	private void wallClimb()
+	{
+		grounded = true;
+		if (Input.GetAxis("Vertical") < 0f)
+		{
+			rigi.gravityScale = 0f;
+
+			transform.Translate(0f, 4f * Time.deltaTime, 0f);
+		}
+		else if (Input.GetAxis("Vertical") > 0f)
+		{
+			rigi.gravityScale = 0f;
+			transform.Translate(0f, -4f * Time.deltaTime, 0f);
+		}
+		isClimbing = true;
+	}
+
+
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "Spikey")
+		{
+			Debug.Log("You Done Died");
+			SceneManager.LoadScene(0);
+		}
+		if (dashing && col.gameObject.tag == "BreakableWall")
+		{
+			UnityEngine.Object.Destroy(col.gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "DashPickup")
+		{
+			dashPickup = true;
+		}
+		if (col.gameObject.tag == "WallClimbPickup")
+		{
+			wallClimbPickup = true;
+		}
+		if (col.gameObject.tag == "DoubleJumpPickup")
+		{
+			doubleJumpPickup = true;
+		}
+	}
+
+	void OnCollisionStay2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "MovingPlatform")
+		{
+			Debug.Log("youre on a moving platform");
+		}
+	}
+}
