eabbac93e429384e581f0bc8ba0d5f569ebd18c9
David Aaron DiManna <ddimanna@users.noreply.github.com>
2016-10-25 17:58:26 +0000

    added the Win UI

--- NanashiProject/Assets/Scripts/UIController.cs
+++ NanashiProject/Assets/Scripts/UIController.cs
@@ -1,16 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour {
 
 	public Text collection;
 
 	public int numberCollected;
+	public GameObject winPanel;
+	//public GameObject player;
+	//public Component playerthings;
 
 	// Use this for initialization
 	void Start () {
 		numberCollected = 0;
+		winPanel.SetActive(false);
+		//player = GetComponent<PlayerController>();
+		//playerthings = GetComponent<PlayerController>();
 	
 	}
 	
@@ -20,6 +27,26 @@ public class UIController : MonoBehaviour {
 
 		collection.text = numberCollected + " / 10"; 
 
+		if(numberCollected >= 10){
+			//playerthings.setActive = false;
+			winPanel.SetActive(true);
+
+
+
+		}
+
 	
 	}
+	public void PlayOnClick(){
+
+		SceneManager.LoadScene(1);
+
+	}
+
+	public void ExitOnClick(){
+
+		SceneManager.LoadScene(0);
+	}
+
+
 }
