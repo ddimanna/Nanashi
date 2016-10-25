f3f4383887b16e093385a746e8da9c9f89a537df
David Aaron DiManna <ddimanna@users.noreply.github.com>
2016-10-25 05:15:12 +0000

    added the UI elements to the main scene, so that it reflects the number of mushrooms that you have collected

---
+++ NanashiProject/Assets/Scripts/UIController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+
+	public Text collection;
+
+	public int numberCollected;
+
+	// Use this for initialization
+	void Start () {
+		numberCollected = 0;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		collection.text = numberCollected + " / 10"; 
+
+	
+	}
+}
