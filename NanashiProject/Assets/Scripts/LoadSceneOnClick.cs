2b0e21c5bb27ddeb26ed0b6367ca9035352d5e13
David Aaron DiManna <ddimanna@users.noreply.github.com>
2016-10-25 01:28:12 +0000

    added in Ryan’s package and set up the scene manager

---
+++ NanashiProject/Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour
+{
+
+public void OnClick()
+
+	{
+		SceneManager.LoadScene (1);
+	}
+
+	void Update(){
+
+		if(Input.GetButtonDown("Start")){
+
+			SceneManager.LoadScene(1);
+		}
+	}
+
+}
