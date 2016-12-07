1cbe3333b5112cf56aacb442e084de9616ac8ba0
David Aaron DiManna <ddimanna@users.noreply.github.com>
2016-12-07 19:14:12 +0000

    added the end cutscene

--- NanashiProject/Assets/Scripts/EnemyController.cs
+++ NanashiProject/Assets/Scripts/EnemyController.cs
@@ -103,10 +103,12 @@ public class EnemyController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		walkAmount.x = walkingDirection * walkSpeed * Time.deltaTime;
-		if (walkingDirection > 0.0f && transform.position.x >= wallRight)
+		if (walkingDirection > 0.0f && transform.position.x >= wallRight){
 			walkingDirection = -1.0f;
-		else if (walkingDirection < 0.0f && transform.position.x <= wallLeft)
+		}
+		else if (walkingDirection < 0.0f && transform.position.x <= wallLeft){
 			walkingDirection = 1.0f;
+		}
 		transform.Translate(walkAmount);
 	}
 
---
+++ NanashiProject/Assets/Scripts/SkipIntroCutscene.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SkipIntroCutscene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetButton("Jump") || Input.GetButton("Start")){
+
+
+			SceneManager.LoadScene("Main");
+		}
+
+		//duplicate the script
+		if(Input.GetKey(KeyCode.Escape)){
+
+			SceneManager.LoadScene("MainMenu1");
+		}
+	
+	}
+}
